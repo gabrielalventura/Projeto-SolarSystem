ad9688a0381ac03b8391f396effd905868f6ee2b
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-09-21 15:12:14 -0300

    Resolução do requisito 3

---
+++ src/components/Title.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+
+    return (
+      <h2>
+        { headline }
+      </h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string,
+};
+
+Title.defaultProps = {
+  headline: '',
+};
+
+export default Title;
